241540113709dd1dfc5c6ef691b6fb53f1759930
Pavlo-Bohdan Ravliv <ravliv.pavlobohdan@gmail.com>
2024-11-04 18:25:39 +0100

    move everything to top level

--- forgedtsx/forgedtsx-plugin/dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import * as ts from "typescript/lib/tsserverlibrary";
-import "./services/refactors";
-import "./services/codefixes";
-declare function init(modules: {
-    typescript: typeof import("typescript/lib/tsserverlibrary");
-}): {
-    create: (info: ts.server.PluginCreateInfo) => ts.LanguageService;
-};
-export = init;
